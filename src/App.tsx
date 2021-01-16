5264b7c26de57e19c51dc2932a384d9457329594
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2021-01-16 04:50:25 -0800

    とりあえず動く、けどいろいろ納得いかない

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 
 import './App.css';
 import Content from './components/Content';
 
-function App(props: any) {
+function App() {
+  const [id, updateFormId] = useState('');
+  const [name, updateFormName] = useState('');
+
   const onChangeId = (e: any) => {
     console.log(e.target.value)
-    props.updateFormId(e.target.value);
+    updateFormId(e.target.value);
   }
 // forName
   const onChangeName = (e: any) => {
     console.log(e.target.value)
-    props.updateFormName(e.target.value);
+    updateFormName(e.target.value);
   }
 // clickSaveButton
   const onSaveItem = () => {
-    console.log(props)
-    console.log("SAVE_ID: ",props.id);
-    console.log("SAVE_NAME: ",props.name);
+    //console.log(props)
+    console.log("SAVE_ID: ",id);
+    console.log("SAVE_NAME: ",name);
   }
 
   //const formItem = this.props
     // formEvent
     //const contentHandler = ({onChangeId, onChangeName, onSaveItem}) => ({ onChangeIdonChangeName, onSaveItem })
-    console.log('App.render:', props);
+    //console.log('App.render:', props);
 
   return (
     <>
       <Content
-        id=''
-        name=''
+        id={id}
+        name={name}
         onChangeId={onChangeId}
         onChangeName={onChangeName}
         onSaveItem={onSaveItem}
